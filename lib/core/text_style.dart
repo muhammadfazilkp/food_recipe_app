56118475eba1ddc348f84933fd8ad91b77d24bbc
Muhammad fasil <fazilfaztml@gmail.com>
2023-10-16 11:15:46 +0530

    ui design started

---
+++ lib/core/text_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle text = GoogleFonts.poppins(
+    textStyle: const TextStyle(color: Colors.black, fontSize: 15,fontWeight: FontWeight.w800));
+
+    TextStyle categori = GoogleFonts.poppins(
+    textStyle: const TextStyle(color: Colors.black, fontSize: 13,));
--- lib/view/screens/home_screen/home_page.dart
+++ lib/view/screens/home_screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipe_app/core/text_style.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,9 +18,44 @@ class HomeScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10)),
                 width: double.infinity,
                 height: 50,
-                child: TextFormField(
-                  decoration: const InputDecoration(border: InputBorder.none),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        border: InputBorder.none, hintText: 'Search Recipe...'),
+                  ),
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Categories',
+                    style: text,
+                  )),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      height: 35,
+                      width: 80,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                          child: Text(
+                        'Breakfast',
+                        style: categori,
+                      )),
+                    ),
+                  )
+                ],
               ),
             )
           ],
